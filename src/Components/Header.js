639226c2ade49306c0cb3c97a48cb96a902d5900
Oğuzhan Ötleş <68377756+oguzz0804@users.noreply.github.com>
2024-01-14 22:49:06 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,7 +14,7 @@ export default function Header() {
     return () => {
       document.querySelector("header.header").removeChild(glitchWindow);
     };
-  }, []); 
+  }, []); //deleted
 
   return (
     <header className="header">
