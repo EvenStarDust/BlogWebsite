9925d05962a049a3ff53bdc05a8aa31063146e67
Oğuzhan Ötleş <oguzz0804@gmail.com>
2024-01-21 22:08:59 +0300

    home feature added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Navbar.css";
+import { IntroSection } from '../Sections/IntroSection'
 
 export default function Navbar({ title }) {
   const [scrolling, setScrolling] = useState(false);
@@ -22,96 +23,61 @@ export default function Navbar({ title }) {
 
   const handleBurgerClick = () => {
     setNavActive(!isNavActive);
-
     handleNavLinksAnimation();
   };
 
   const handleNavLinksAnimation = () => {
     const navLinks = document.querySelectorAll(".nav-links li");
-
     navLinks.forEach((link, index) => {
       if (isNavActive) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3
+          }s`;
       }
     });
   };
 
   return (
     <>
-      <nav className={scrolling ? "nav-opacity" : ""}>
-        <div className="logo">
-          <h4>{title}</h4>
-        </div>
-        <ul className={`nav-links ${isNavActive ? "nav-activ" : ""}`}>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Projects</a>
-          </li>
-          <li>
-            <a href="#">Men</a>
-          </li>
-          <li>
-            <a href="#">Women</a>
-          </li>
-          <li>
-            <a href="#">Kids</a>
-          </li>
-          <li>
-            <a href="#">Accesories</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-        </ul>
-        <div
-          className={`burger ${isNavActive ? "toggle" : ""}`}
-          onClick={handleBurgerClick}
-        >
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-      </nav>
-
+      <section className={scrolling ? "scrolling__nav " : ''}>
+        <nav className={scrolling ? "nav-opacity" : ""}>
+          <div className="logo">
+            <h4>{title}</h4>
+          </div>
+          <ul className={`nav-links ${isNavActive ? "nav-activ" : ""}`}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Categories</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+          </ul>
+          <div
+            className={`burger ${isNavActive ? "toggle" : ""}`}
+            onClick={handleBurgerClick}
+          >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+          </div>
+        </nav>
+      </section>
       <div className="banner">
         <div className="text">
           <h1></h1>
         </div>
       </div>
-      <div></div>
-
-      <div class="section1">
-        <p>
-          <br/>
-         <p>Merhaba Bilgi Sevdalıları! </p> 
-         <br/>
-          Evenstar dünyasına hoş geldiniz! Burası,
-          merakınızı tetikleyecek, sizi bilgiyle buluşturacak ve farklı
-          konularda keşifler yapmanıza olanak tanıyacak bir platform. Uzayın
-          derinliklerinden bilgisayar dünyasına, sıra dışı hayvan türlerinden
-          güncel sosyal medya trendlerine kadar pek çok konuda bilgiye açılan
-          kapı burada. Evenstar, herkesin ilgisini çekecek geniş bir içerik
-          yelpazesi sunuyor. Sitemizde kaybolurken, farklı konular hakkında
-          bilgi edinirken, ilginç ve eğlenceli detayları keşfederken zamanın
-          nasıl geçtiğini anlamayacaksınız. Yenilikçi yazılar, ilham verici
-          içerikler ve sizi düşündürecek makalelerle dolu olan sitemizde her gün
-          yeni bir şey öğrenme fırsatı sizi bekliyor. Bu platformda her şeyi
-          bulabilir, öğrenebilir ve paylaşabilirsiniz. Siz de Evenstar'a
-          katılarak, bilgi dolu bir dünyanın kapılarını aralayın. Unutmayın,
-          keşfetmek için asla geç değildir! Bilgi dolu günler dileriz! 
-          <p>Evenstar
-          Ekibi</p>
-        </p>
-      </div>
+      <IntroSection />
     </>
   );
 }
---
+++ src/Components/Sections/IntroSection.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect, useRef } from "react";
+import './IntroSection.css'
+
+export const IntroSection = () => {
+    const { word, stop, start, color } = useTypingText(
+        ["", "Evenstar Ekibi"],
+        130,
+        20,
+    );
+    return (
+        <section>
+            <div className="d-table w-100 text-start text-lg-center">
+                <div className="div1">
+                    <div className="container">
+                        <h1 className="h1"><p>
+                            <p>Merhaba Bilgi Sevdalıları! </p>
+                            <br />
+                            Evenstar dünyasına hoş geldiniz! Burası,
+                            merakınızı tetikleyecek, sizi bilgiyle buluşturacak ve farklı
+                            konularda keşifler yapmanıza olanak tanıyacak bir platform. Uzayın
+                            derinliklerinden bilgisayar dünyasına, sıra dışı hayvan türlerinden
+                            güncel sosyal medya trendlerine kadar pek çok konuda bilgiye açılan
+                            kapı burada. Evenstar, herkesin ilgisini çekecek geniş bir içerik
+                            yelpazesi sunuyor. Sitemizde kaybolurken, farklı konular hakkında
+                            bilgi edinirken, ilginç ve eğlenceli detayları keşfederken zamanın
+                            nasıl geçtiğini anlamayacaksınız. Yenilikçi yazılar, ilham verici
+                            içerikler ve sizi düşündürecek makalelerle dolu olan sitemizde her gün
+                            yeni bir şey öğrenme fırsatı sizi bekliyor. Bu platformda her şeyi
+                            bulabilir, öğrenebilir ve paylaşabilirsiniz. Siz de Evenstar'a
+                            katılarak, bilgi dolu bir dünyanın kapılarını aralayın. Unutmayın,
+                            keşfetmek için asla geç değildir! Bilgi dolu günler dileriz!
+                            <br /><br />
+                            {word}</p></h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+const FORWARD = "forward";
+const BACKWARD = "backward";
+
+const useTypingText = (words, keySpeed = 1000, maxPauseAmount = 10, color = 'red') => {
+    const [wordIndex, setWordIndex] = useState(0);
+    const [currentWord, setCurrentWord] = useState(words[wordIndex].split(""));
+    const [isStopped, setIsStopped] = useState(false);
+    const direction = useRef(BACKWARD);
+    const typingInterval = useRef();
+    const letterIndex = useRef();
+
+    const stop = () => {
+        clearInterval(typingInterval.current);
+        setIsStopped(true);
+    };
+
+    useEffect(() => {
+        // Start at 0
+        let pauseCounter = 0;
+
+        if (isStopped) return;
+
+        const typeLetter = () => {
+            if (letterIndex.current >= words[wordIndex].length) {
+                direction.current = BACKWARD;
+
+                // Begin pause by setting the maxPauseAmount prop equal to the counter
+                pauseCounter = maxPauseAmount;
+                return;
+            }
+
+            const segment = words[wordIndex].split("");
+            setCurrentWord(currentWord.concat(segment[letterIndex.current]));
+            letterIndex.current = letterIndex.current + 1;
+        };
+
+        const backspace = () => {
+            if (letterIndex.current === 0) {
+                const isOnLastWord = wordIndex === words.length - 1;
+
+                setWordIndex(!isOnLastWord ? wordIndex + 1 : 0);
+                direction.current = FORWARD;
+
+                return;
+            }
+
+            const segment = currentWord.slice(0, currentWord.length - 1);
+            setCurrentWord(segment);
+            letterIndex.current = currentWord.length - 1;
+        };
+
+        typingInterval.current = setInterval(() => {
+            // Wait until counter hits 0 to do any further action
+            if (pauseCounter > 0) {
+                pauseCounter = pauseCounter - 1;
+                return;
+            }
+
+            if (direction.current === FORWARD) {
+                typeLetter();
+            } else {
+                backspace();
+            }
+        }, keySpeed);
+
+        return () => {
+            clearInterval(typingInterval.current);
+        };
+    }, [currentWord, wordIndex, keySpeed, words, maxPauseAmount, isStopped]);
+
+    return {
+        word: (
+            <span style={{ color: color }}>
+                <span>{currentWord.length ? currentWord.join("") : ""}</span>
+            </span>
+        ),
+        start: () => setIsStopped(false),
+        stop
+    };
+};
