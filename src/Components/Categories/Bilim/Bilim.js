118fe524154192d2cf3b0ba37b450d999d3743b0
bilgekosee <bilge.koosee@gmail.com>
2024-09-21 11:58:03 +0300

    yeni css güncellemeleri

--- src/Components/Categories/Bilim/Bilim.js
+++ src/Components/Categories/Bilim/Bilim.js
@@ -1,20 +1,47 @@
 import bilimData from "../../Data/bilimData.json";
 import "./Bilim.css";
+import img1 from "../../Data/photo/1.jpeg";
+import img2 from "../../Data/photo/2.jpg";
+import img3 from "../../Data/photo/3.jpg";
+import img4 from "../../Data/photo/4.jpg";
+import img5 from "../../Data/photo/5.jpg";
+import img6 from "../../Data/photo/6.jpg";
+import img7 from "../../Data/photo/7.jpg";
+import img8 from "../../Data/photo/8.jpg";
+const imageMap = {
+  1: img1,
+  2: img2,
+  3: img3,
+  4: img4,
+  5: img5,
+  6: img6,
+  7: img7,
+  8: img8,
+};
 
 const Bilim = () => {
   return (
     <div className="bilimContainer">
-      <h1 className="bilimbaslik">BİLİM YAZILARI</h1>
+      <div class="col-md-12 text-center">
+        <h3 class="animate-charcter"> BİLİM YAZILARI</h3>
+      </div>
+
       <ul>
         {bilimData && bilimData.length > 0 ? (
           bilimData.map((article) => (
             <li key={article.id}>
-              <div>
-                <h2>{article.title}</h2>
-                <img src="/photo/1.jpg" alt={article.title || "image"} />
-              </div>
+              <div className="textImageContainer">
+                <img
+                  className="imgBilim-container"
+                  src={imageMap[article.id]}
+                  alt={article.title || "image"}
+                />
 
-              <p>{article.description}</p>
+                <div>
+                  <h2>{article.title}</h2>
+                  <p>{article.description}</p>
+                </div>
+              </div>
             </li>
           ))
         ) : (
